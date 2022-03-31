850e8e78060b77de74b950734f9de44db3145997
LuisBamba <luis@vivebamba.com>
2022-03-30 18:30:04 -0600

    Entrypoint Tailwind Fix

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+python manage.py migrate
+python manage.py tailwind install
+cd theme/static_src/
+npm audit fix
+cd ../../
+python manage.py tailwind build
 python manage.py collectstatic --no-input
-python manage.py tailwind build --no-input
 
-uwsgi --socket :8000 --master --enable-threads --module bi.wsgi
-
-
-
+uwsgi --socket :8000 --master --enable-threads --module bi.wsgi -i
